5f89912ecdd755c2ecaab6e87a994c5c0f972880
SURI <101981952+SURI913@users.noreply.github.com>
2023-06-08 02:47:56 +0900

    튜토리얼 제작 완료

--- Assets/Script/Dialog.cs
+++ Assets/Script/Dialog.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using UnityEditor.Rendering.PostProcessing;
 
 
 public class Dialog : MonoBehaviour, IPointerDownHandler
--- Assets/Script/UI&Data/Tutorial/TutorialDialog.cs
+++ Assets/Script/UI&Data/Tutorial/TutorialDialog.cs
@@ -6,6 +6,16 @@ public class TutorialDialog : TutorialBase
 	// 캐릭터들의 대사를 진행하는 DialogSystem
 	private	DialogSystem dialogSystem;
 
+	//잠깐 꺼두는 상점 오브젝트
+	[SerializeField]
+	private GameObject shopItem1 = null;
+	[SerializeField]
+	private GameObject shopItem2 = null;
+	[SerializeField]
+	private GameObject shopItem3 = null;
+	[SerializeField]
+	private GameObject shopItem4 = null;
+
 	public override void Enter()
 	{
 		dialogSystem = GetComponent<DialogSystem>();
@@ -16,10 +26,22 @@ public class TutorialDialog : TutorialBase
 	{
 		// 현재 분기에 진행되는 대사 진행
 		bool isCompleted = dialogSystem.UpdateDialog();
+		if(shopItem1 != null && shopItem2 != null && shopItem3 != null && shopItem4 != null){
+			shopItem1.SetActive(false);
+			shopItem2.SetActive(false);
+			shopItem3.SetActive(false);
+			shopItem4.SetActive(false);
+		}
 
 		// 현재 분기의 대사 진행이 완료되면
 		if ( isCompleted == true )
 		{
+			if(shopItem1 != null && shopItem2 != null && shopItem3 != null && shopItem4 != null){
+				shopItem1.SetActive(true);
+				shopItem2.SetActive(true);
+				shopItem3.SetActive(true);
+				shopItem4.SetActive(true);
+			}
 			// 다음 튜토리얼로 이동
 			controller.SetNextTutorial();
 		}
