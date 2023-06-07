5a5c8057e97d99f50eddf01be138e8835f4a1019
tenbaby <110656858+tenbaby@users.noreply.github.com>
2023-06-07 23:48:06 +0900

    .
    .

--- Assets/Script/Boss/layer.cs
+++ Assets/Script/Boss/layer.cs
@@ -36,31 +36,8 @@ public class layer : MonoBehaviour
     {
         while (true)
         {
-            // 색상을 하얀색에서 빨간색으로
-            //yield return StartCoroutine(ColorLerp(Color.white, Color.red));
-            // 색상을 빨간색에서
-            // yield return StartCoroutine(ColorLerp(Color.red, Color.white));
             yield return StartCoroutine("Invincibility");
         }
-    }
-    private IEnumerator ColorLerp(Color startColor, Color endColor)
-    {
-        float currentTime = 0.0f;
-        float percent = 0.0f;
-
-        while(percent < 1)
-        {
-            // lerpTime 시간동안 while() 반복문 실행
-            currentTime += Time.deltaTime;
-            percent = currentTime / lerpTime;
-
-            sprite.color = Color.Lerp(startColor, endColor, percent);
-
-            yield return null;
-        }
-        sprite.color = endColor;
-        yield return new WaitForSeconds(1f);
-        Destroy(sprite.gameObject);
     }
 
     IEnumerator Invincibility()  // 색깔 깜빡임
