b1fe2b95f68245ba4ec5e89008c49d5268abe748
hayum0525 <khyoung9128@gmail.com>
2023-06-08 05:43:40 +0900

    다이얼로그 와 스테이지 연결

--- Assets/Script/DialogTest.cs
+++ Assets/Script/DialogTest.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogTest : MonoBehaviour
 {
@@ -11,35 +12,46 @@ public class DialogTest : MonoBehaviour
     private DialogSystem dialogSystem02;
     [SerializeField]
     private TextMeshProUGUI textCountdown;
-    
+
+    public DialogSystem dialogScene;
+    [SerializeField]
+    private string nextSceneName = "";
+
 
     private IEnumerator Start()
     {
         textCountdown.gameObject.SetActive(false);
         //첫번쨰 시작 
-       
+
         yield return new WaitUntil(() => dialogSystem01.UpdateDialog());
 
-        //textCountdown.gameObject.SetActive(true);
-        //int count = 2;
-        //while (count > 0)
-        //{
-        //    textCountdown.text = count.ToString();
-        //    count--;
-        //    yield return new WaitForSeconds(1);
-        //    textCountdown.gameObject.SetActive(false);
-        //}
-        //textCountdown.gameObject.SetActive(false);
 
-        //두번째 시작
-        
-        yield return new WaitUntil(() => dialogSystem02.UpdateDialog());
 
         textCountdown.gameObject.SetActive(true);
-        textCountdown.text = "GAME START";
+        int count = 2;
+        while (count > 0)
+        {
+            //textCountdown.text = count.ToString();
+            count--;
+            yield return new WaitForSeconds(1);
+            textCountdown.gameObject.SetActive(false);
+        }
+        textCountdown.gameObject.SetActive(false);
 
-        yield return new WaitForSeconds(2);
+        //두번째 시작
+        if (dialogSystem02)
+        {
+            yield return new WaitUntil(() => dialogSystem02.UpdateDialog());
+        }
+
+        SceneManager.LoadScene(nextSceneName);
+
+        yield return new WaitForSeconds(1);
+
 
         UnityEditor.EditorApplication.ExitPlaymode();
-        }
+
+       
+    }
+   
 }
